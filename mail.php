a17a3bb68fd2a9f78a20a06018bbdcaf541c41f3
l0hitk <lohit.k2307@gmail.com>
2026-04-09 11:58:18 +0530

    REWORK

--- mail.php
+++ mail.php
@@ -1,71 +1,66 @@
 <?php
-if(!empty($_REQUEST['Name']) && !empty($_REQUEST['Email']) && !empty($_REQUEST['Phone'])){
-    $name = $_REQUEST['Name'];
-    echo $email = $_REQUEST['Email'];
-    echo $fname=explode(" ", $name);
-    echo $phone  = $_REQUEST['Phone'];
-    echo $mes  = $_REQUEST['Message'];
-    echo $text=$_REQUEST['Company'];
-    
-    // ini_set("SMTP", "mail.milkywave.in");
-    ini_set("sendmail_from", "info@lumos.in");
-    
-    $to="vidya@lumos.in";
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= 'From:info@lumos.in <info@lumos.in>' . "\r\n";
-    
-    $headers .= 'CC: vidya@lumos.in' . "\r\n";
-    
-    $headers1 = "MIME-Version: 1.0" . "\r\n";
-    $headers1 .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers1 .= 'From:info@lumos.in <info@lumos.in>' . "\r\n";
-
-    $subject1 = "[Balaji Electro Controls Pvt. Ltd.] - You have a new Business enquiry!";
-    $subject2 = "[Balaji Electro Controls Pvt. Ltd.] - Thanks for contacting us!";
-    $message1 = "<html>
-    <head>
-    <title></title>
-    </head>
-    <body>
-    Dear Team,<br/><br/>
-    You have a new business enquiry.<br/><br/>
-    Name: $name<br/>
-    Phone: $phone<br/>
-    Email: $email<br/>
-    Message: $mes<br/>
-    EnquiryType: $type<br/><br/>
-    Regards,<br/>
-    Balaji Electro Controls Pvt. Ltd.
-    
-    </body>
-    </html>";
-
-     $message2 = "<html>
-    <head>
-    <title></title>
-    </head>
-    <body>
-    Hi $fname[0],<br/><br/>
-    Thank you for contacting us. we will get back to you as soon as possible.<br/>
-    <br/><br/>
-    Regards,<br/>
-    Balaji Electro Controls Pvt. Ltd.
-
-    
-    </body>
-    </html>";
-    // echo $message2;die;
-    mail($email,$subject2,$message2,$headers1);
-    mail($to,$subject1,$message1,$headers);
-        
-     //header('Location: Thank-You.html');
-      
+ 
+session_start();
+ 
+if($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $email_title = "";
+    $concerned_department = "";
+    $visitor_message = "";
+ 
+    if(isset($_POST['captcha_challenge']) && $_POST['captcha_challenge'] == $_SESSION['captcha_text']) {
      
-     ?>
+        if(isset($_POST['visitor_name'])) {
+            $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+        }
+         
+        if(isset($_POST['visitor_email'])) {
+            $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+            $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+             
+        }
+         
+        if(isset($_POST['email_title'])) {
+            $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
+        }
+         
+        if(isset($_POST['concerned_department'])) {
+            $concerned_department = filter_var($_POST['concerned_department'], FILTER_SANITIZE_STRING);
+        }
+         
+        if(isset($_POST['visitor_message'])) {
+            $visitor_message = htmlspecialchars($_POST['visitor_message']);
+        }
+         
+        if($concerned_department == "billing") {
+            $recipient = "billing@domain.com";
+        }
+        else if($concerned_department == "marketing") {
+            $recipient = "marketing@domain.com";
+        }
+        else if($concerned_department == "technical support") {
+            $recipient = "tech.support@domain.com";
+        }
+        else {
+            $recipient = "contact@domain.com";
+        }
+         
+        $headers  = 'MIME-Version: 1.0' . "\r\n"
+        .'Content-type: text/html; charset=utf-8' . "\r\n"
+        .'From: ' . $visitor_email . "\r\n";
+         
+        if(mail($recipient, $email_title, $visitor_message, $headers)) {
+            echo '<p>Thank you for contacting us. You will get a reply within 24 hours.</p>';
+        } else {
+            echo '<p>We are sorry but the email did not go through.</p>';
+        }
+    } else {
+        echo '<p>You entered an incorrect Captcha.</p>';
+    }
      
-     <meta http-equiv="refresh" content="0;URL=thank-you.html" />
-     
-<?php    }
-     
- ?>
+} else {
+    echo '<p>Something went wrong</p>';
+}
+ 
+?>
